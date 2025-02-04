de906a402604ed18bf410874ef39cbbce6b709e2
SoullessScythe <pikapika64.ac@gmail.com>
2025-02-03 19:21:58 -0500

    Jumping Enemy

---
+++ Assets/_Scripts/Enemy Scripts/BasicJumpEnemy.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BasicJumpEnemy : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject enemy;
+
+    [SerializeField] private float atkFrequency;
+
+    [SerializeField] private float forwardVelocity;
+    [SerializeField] private float upwardVelocity;
+
+    private Rigidbody enemyRB;
+    private float timer;
+    private float delay;
+
+    private bool facingRight;
+    private bool facingLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        timer = 0;
+
+        //InvokeRepeating("jumpAttack", 3f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        facePlayer();
+
+
+        //jumpAttack();
+        
+
+        timer += Time.deltaTime;
+        delay += Time.deltaTime;
+        
+        while (timer >= atkFrequency) {
+            jumpAttack();
+            timer -= atkFrequency;
+        }
+    }
+
+
+    void facePlayer() {
+        if (player.transform.position.x > enemy.transform.position.x) {
+            enemy.transform.rotation = Quaternion.Euler(0, 90, 0);
+            facingRight = true;
+            facingLeft = false;
+
+        } else if (player.transform.position.x < enemy.transform.position.x) {
+            enemy.transform.rotation = Quaternion.Euler(0, 270, 0);
+            facingRight = false;
+            facingLeft = true;
+        }
+    }
+
+    void jumpAttack() {
+
+        delay = 0;
+        enemyRB = GetComponent<Rigidbody>();
+
+        if(facingRight) {
+            Debug.Log("jumped");
+
+            enemyRB.velocity = new Vector3(forwardVelocity, upwardVelocity, 0);
+        }
+        else if(facingLeft) {
+            Debug.Log("jumped");
+
+            enemyRB.velocity = new Vector3(-forwardVelocity, upwardVelocity, 0);
+
+        }
+
+
+        /*
+         * enemyRB.AddForce(Vector3.up * 400f);
+
+
+        enemyRB.AddForce(Vector3.forward * 50f);
+        */
+    }
+
+}
