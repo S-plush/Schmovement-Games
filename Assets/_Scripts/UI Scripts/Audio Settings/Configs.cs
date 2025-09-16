7e04b814b13942340107fbe37d80a71984685c08
S Plush <48722178+S-plush@users.noreply.github.com>
2025-09-16 18:22:46 -0400

    mana rock and prisoner cage created

--- Assets/_Scripts/UI Scripts/Audio Settings/Configs.cs
+++ Assets/_Scripts/UI Scripts/Audio Settings/Configs.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 public class Configs : MonoBehaviour
 {
     public float volBG, volSFX;
-    public bool optionsGUI;
-    public Rect optionsRect = new Rect(20, 20, 800, 800);
     AudioConfig audioConfig;
     public Button SaveButton;
 
@@ -21,18 +19,18 @@ public class Configs : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audioConfig = GetComponent<AudioConfig>();
         LoadAll();
         //player = Alpha.PlayerRef;
-        sfxSlider = GetComponent<Slider>();
-        bgmSlider = GetComponent<Slider>();
+        //sfxSlider = GetComponent<Slider>();
+        //bgmSlider = GetComponent<Slider>();
+        bgmObject = GameObject.Find("Music Volume");
+        sfxObject = GameObject.Find("Sound Effects Volume");
+        bgmSlider = bgmObject.GetComponent<Slider>();
+        sfxSlider = sfxObject.GetComponent<Slider>();
         bgmSlider.value = volBG;
         sfxSlider.value = volSFX;
-        //bgmObject = GameObject.Find("Music Volume");
-        //sfxObject = GameObject.Find("Sound Effects Volume");
-        //bgmSlider = bgmObject.GetComponent<Slider>();
-        //sfxSlider = sfxObject.GetComponent<Slider>();
 
-        audioConfig = GetComponent<AudioConfig>();
         audioConfig.SetAll();
         Application.targetFrameRate = -1;
     }
@@ -48,18 +46,13 @@ public class Configs : MonoBehaviour
     //    }
     //}
 
-    public void OpenOptions()
-    {
-        optionsGUI = !optionsGUI;
-    }
-
     public void BGMSliderChange(float value)
     {
         volBG = value;
         PlayerPrefs.SetFloat("bgmVolume", volBG);
         //Debug.Log("slider changed new volBG: " + volBG);
         SetAllAudio();
-        //SaveAll();
+        SaveAll();
     }
 
     public void SFXSliderChange(float value)
@@ -67,7 +60,7 @@ public class Configs : MonoBehaviour
         volSFX = value;
         PlayerPrefs.SetFloat("sfxVolume", volSFX);
         SetAllAudio();
-        //SaveAll();
+        SaveAll();
     }
 
     public void SaveAll()
