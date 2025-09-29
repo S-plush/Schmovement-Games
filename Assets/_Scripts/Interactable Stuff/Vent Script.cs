8d50c2db2dd86afa5c311494dcd3a150e3013cb7
LukeMan2015 <147107486+LukeMan2015@users.noreply.github.com>
2025-09-29 05:17:47 -0400

    Vents
    (check discord)

---
+++ Assets/_Scripts/Interactable Stuff/Vent Script.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class VentScript : MonoBehaviour
+{
+    //[SerializeField] private Animator ventActivated; //UPDATE THIS ANIMATOR VARIABLE CUZ IT AINT DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    public static bool playerInside = false;
+
+    public GameObject ventSelectUI;
+
+    public GameObject HUD;
+
+    private VentsToFile VentsToFileScript;
+
+    private MiscDataToFile MiscDataToFileScript;
+
+    public GameObject CampButton;
+    public GameObject Detention1Button;
+    public GameObject Storage1Button;
+
+    //ADD YOUR NEW BUTTON AS A VARIABLE ABOVE THIS LINE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    public void Start()
+    {
+        playerInside = false;
+
+        HUD = Alpha.FindInScene("Main HUD Group");
+
+        VentsToFileScript = FindObjectOfType<VentsToFile>(); //initilize VentsToFileScript with the actual script
+
+        MiscDataToFileScript = FindObjectOfType<MiscDataToFile>(); //initilize MiscDataToFileScript with the actual script
+    }
+
+    private void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.R) || (playerInside && ventSelectUI.activeSelf && Input.GetKeyDown(KeyCode.Escape))) //checks if player is interacting with the vent or pressing ESC to leave the vent menu
+        {
+            //ventActivated.Play("Activating vent", -1, 0f); //UPDATE THIS IF ANIMATION PEOPLES ARE READING
+
+            if (ventSelectUI.activeSelf)
+            {
+                HUD.SetActive(true);
+                ventSelectUI.SetActive(false);
+                Time.timeScale = 1.0f;
+                Alpha.isGamePaused = false;
+            }
+            else
+            {
+                HUD.SetActive(false);
+                ventSelectUI.SetActive(true);
+                Time.timeScale = 0.0f;
+                Alpha.isGamePaused = true;
+
+                if (VentsToFileScript.HasDetention1Vent == false && this.gameObject.name == "D1 Vent Interactable")
+                {
+                    VentsToFileScript.HasDetention1Vent = true;
+                }
+                if (VentsToFileScript.HasElevatorStorageVent == false && this.gameObject.name == "Storage1 Vent Interactable")
+                {
+                    VentsToFileScript.HasElevatorStorageVent = true;
+                }
+
+                //ADD NEW VENTS AS AN IF STATEMENT ABOVE THIS LINE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+                VentsToFileScript.saveAllVentData(); //saves the goods from just before
+
+                if (VentsToFileScript.HasCampVent == true)
+                {
+                    CampButton.SetActive(VentsToFileScript.HasCampVent);
+                }
+                if(VentsToFileScript.HasDetention1Vent == true)
+                {
+                    Detention1Button.SetActive(VentsToFileScript.HasDetention1Vent);
+                }
+                if (VentsToFileScript.HasElevatorStorageVent == true)
+                {
+                    Storage1Button.SetActive(VentsToFileScript.HasElevatorStorageVent);
+                }
+
+                //ADD NEW VENTS AS AN IF STATEMENT ABOVE THIS LINE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("player is in the trigger point");
+
+        if (other.gameObject.tag == "Player")
+        {
+            playerInside = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerInside = false;
+        }
+    }
+
+    ///////stuff for UI buttons below here///////////////////////////////////////////////////
+
+    public void LoadSceneOfPushedButton()
+    {
+        GameObject clickedButton = EventSystem.current.currentSelectedGameObject;
+
+        if (clickedButton.name == "CampButton")
+        {
+            RespawnPoint.currentCheckpointName = "Camp Vent Transition";
+            RespawnPoint.currentCheckpointSceneName = "CampArea";
+        }
+        else if (clickedButton.name == "Detention1Button")
+        {
+            RespawnPoint.currentCheckpointName = "D1 Vent Transition";
+            RespawnPoint.currentCheckpointSceneName = "DetentionCenter";
+        }
+        else if (clickedButton.name == "Elevators/Storage")
+        {
+            RespawnPoint.currentCheckpointName = "Storage1 Vent Transition";
+            RespawnPoint.currentCheckpointSceneName = "StorageSector1";
+        }
+
+        //ADD NEW VENTS AS AN ELSE IF STATEMENT ABOVE THIS LINE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        
+        MiscDataToFileScript.saveAllMiscData();
+        SceneManager.LoadScene(RespawnPoint.currentCheckpointSceneName);
+    }
+}
